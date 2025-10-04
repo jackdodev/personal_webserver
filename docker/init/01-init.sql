56ba6b06fb2a3dc94e896a6d2037cd6122900447
Jaekuk Kim <jackdodev@gmail.com>
2025-10-03 22:58:02 -0700

    update docker-compose for dev env

--- docker/init/01-init.sql
+++ docker/init/01-init.sql
@@ -12,3 +12,18 @@ INSERT INTO blogs (blog_id, subject, content_path, created_at, last_modified) VA
 'blog2_id', 'blog2_name', 'blog2_content_path', NOW(), NOW());
 INSERT INTO blogs (blog_id, subject, content_path, created_at, last_modified) VALUES (
 'blog3_id', 'blog3_name', 'blog3_content_path', NOW(), NOW());
+
+CREATE TABLE IF NOT EXISTS projects (
+project_id varchar(255) PRIMARY KEY,
+name varchar(255) NOT NULL,
+content_path varchar(255) NOT NULL,
+created_at TIME NOT NULL,
+last_modified TIME NOT NULL
+);
+
+INSERT INTO projects (project_id, name, content_path, created_at, last_modified) VALUES (
+'proj1_id', 'proj1_name', 'proj1_content_path', '08:00:00', '08:00:00');
+INSERT INTO projects (project_id, name, content_path, created_at, last_modified) VALUES (
+'proj2_id', 'proj2_name', 'proj2_content_path', '09:00:00', '09:00:00');
+INSERT INTO projects (project_id, name, content_path, created_at, last_modified) VALUES (
+'proj3_id', 'proj3_name', 'proj3_content_path', '10:00:00', '10:00:00');
